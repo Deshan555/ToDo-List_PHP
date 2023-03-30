47454b4b85798d67f73186d8c2ce31325dc8f6da
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2023-03-30 22:54:33 +0530

    add google font CDN link and import google fonts

--- index.php
+++ index.php
@@ -28,6 +28,8 @@ $todos = $stmt->get_result();
   
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
   
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
   <title>ToDo List</title>
   
   <link href="style.css" rel="stylesheet">
@@ -116,9 +118,7 @@ $todos = $stmt->get_result();
                           </td>
                           
                           <td>
-                            <button type="button" class="btn btn-danger btn-sm px-3">
-                              <i class="fas fa-times"></i>
-                            </button>
+                            <i class="material-icons" style="color:red;">delete</i>
                           </td>
                         
                         </tr>
