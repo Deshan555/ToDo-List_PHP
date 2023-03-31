8557a594c52ec76428a390d565792b9f9cc39c5e
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2023-03-31 20:36:52 +0530

    finalize delete task backend

--- delete.php
+++ delete.php
@@ -1,10 +1,19 @@
-<?
+<?php
 
-$id = $_POST['task_id'];
+include('config.php');
 
-echo $id;
+$id = $_POST['delete_id'];
 
+$sql = "DELETE FROM todo_list WHERE ID = $id;";
 
+if($conn->query($sql) === TRUE) 
+{
+  header('location: index.php');
+}else
+{
+  echo "Error deleting record: " . $conn->error;
+}
 
+$conn->close();
 
 ?>
